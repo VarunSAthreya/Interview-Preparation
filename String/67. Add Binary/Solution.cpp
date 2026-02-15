f6463318f5060f4c4ab8e2984d5177aa9a89030a
Varun Athreya <varun.athreya@contentstack.com>
2026-02-16 02:34:38 +0530

    67. Add Binary

---
+++ String/67. Add Binary/Solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+
+        string result = "";
+
+        while(i>=0 || j>=0 || carry == 1) {
+            int sum = carry;
+            if(i>=0) {
+                sum += a[i] - '0';
+                i--;
+            }
+            if(j>=0) {
+                sum += b[j] - '0';
+                j--;
+            }
+
+            result = to_string(sum%2) + result;
+            carry = sum/2;
+        } 
+
+        return result;
+    }
+};
