36ca7b3b46fe33030704f7008ecf3801d687863b
Varun Athreya <varun.athreya@contentstack.com>
2026-02-16 03:27:08 +0530

    611. Valid Triangle Number

---
+++ Arrays/611. Valid Triangle Number/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int res = 0;
+
+        for(int i =2; i<n;i++) {
+            int l =0,r = i-1;
+
+            while(l < r) {
+                if(nums[l]+nums[r] > nums[i]) {
+                    res += (r-l);
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
